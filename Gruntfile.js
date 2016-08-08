1d466bce8c6064c1a457f130a64133b7c486244a
Rick Waldron <waldron.rick@gmail.com>
2016-08-08 12:58:43 -0400

    Grunt: "changelog" task
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,18 @@
+// System Objects
+var cp = require("child_process");
+var path = require("path");
+
+// Third Party Dependencies
+var tags = require("common-tags");
+
+
 module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
     nodeunit: {
       tests: [
-        "test/tessel.js"
+        "test/tessel-io.js"
       ]
     },
     jshint: {
@@ -61,4 +69,46 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-git-authors");
 
   grunt.registerTask("default", ["jshint", "jscs", "nodeunit"]);
+
+
+
+  grunt.registerTask("changelog", "`changelog:0.0.0--0.0.2` or `changelog`", (range) => {
+    var done = grunt.task.current.async();
+
+    if (!range) {
+      // grunt changelog
+      range = cp.execSync("git tag --sort version:refname").toString().split("\n");
+    } else {
+      // grunt changelog:previous--present
+      range = range.split("--");
+    }
+
+    range = range.filter(Boolean).reverse();
+
+    cp.exec(`git log --format="|%h|%s|" ${range[1]}..${range[0]}`, (error, result) => {
+      if (error) {
+        console.log(error.message);
+        return;
+      }
+
+      var rows = result.split("\n").filter(commit => {
+        return !commit.includes("|Merge ") && !commit.includes(range[0]);
+      }).join("\n");
+
+      // Extra whitespace above and below makes it easier to quickly copy/paste from terminal
+      grunt.log.writeln(`\n\n${changelog(rows)}\n\n`);
+
+      done();
+    });
+  });
+
 };
+
+function changelog(rows) {
+  return tags.stripIndent `
+| Commit | Message/Description |
+| ------ | ------------------- |
+${rows}
+`;
+}
+
